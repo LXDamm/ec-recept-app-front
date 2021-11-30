01d38743a6d93409d708a8fc6d3e5f8f64a662f5
ZARA <zahraalemi@gmail.com>
2021-11-30 14:10:13 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
 import './App.css';
 import { SideBar } from './components/SideBar';
-import { StartPage } from './components/StartPage';
+import React from 'react';
+import {StartPage} from './components/StartPage';
 import { UserLogin } from './components/User/UserLogin';
+import { BrowserRouter,Router, Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
    <div className="appContainer">
      <SideBar/>
-     <UserLogin/>
-     <StartPage />
+     <BrowserRouter>
+       <Routes>
+          <Route exact path="/" component={StartPage}  />
+          <Route path="/Member" component={UserLogin} />
+      </Routes>
+      </BrowserRouter>
    </div>
   );
 }
