1825b48f83528585f8afc6e2b9255392f80931bd
Lexx Damm <lexx.damm@gmail.com>
2021-11-30 14:24:59 +0100

    Route instead of Switch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-
-import './App.css';
-import { SideBar } from './components/SideBar';
-import React from 'react';
-import {StartPage} from './components/StartPage';
-import { UserLogin } from './components/User/UserLogin';
-import { BrowserRouter,Router, Routes, Route } from 'react-router-dom';
-
-
+import "./App.css";
+import { SideBar } from "./components/SideBar";
+import React from "react";
+import { StartPage } from "./components/StartPage";
+import { UserLogin } from "./components/User/UserLogin";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-   <div className="appContainer">
-     <SideBar/>
-     <BrowserRouter>
-       <Routes>
-          <Route exact path="/" component={StartPage}  />
-          <Route path="/Member" component={UserLogin} />
-      </Routes>
-      </BrowserRouter>
-   </div>
+    <div className="appContainer">
+      <Router>
+        <SideBar />
+        <Routes>
+          <Route exact path="/" element={<StartPage/>} />
+          <Route path="/Member" element={<UserLogin/>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
